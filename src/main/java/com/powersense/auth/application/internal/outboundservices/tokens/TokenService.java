d9ac079dfe8c9a957484121d9178159e84ebd9d0
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-12-02 04:59:12 -0500

    feat: add tokenservice

--- src/main/java/com/powersense/auth/application/internal/outboundservices/tokens/TokenService.java
+++ src/main/java/com/powersense/auth/application/internal/outboundservices/tokens/TokenService.java
@@ -3,8 +3,16 @@ package com.powersense.auth.application.internal.outboundservices.tokens;
 import com.powersense.auth.domain.model.aggregates.User;
 
 public interface TokenService {
+
+    // Genera un token JWT basado en la información del usuario
     String generateToken(User user);
+
+    // Extrae el ID del usuario contenido dentro del token
     Long extractUserId(String token);
+
+    // Verifica si el token es válido (firma, expiración, estructura)
     boolean validateToken(String token);
+
+    // Crea un nuevo token basado en uno existente (generalmente cuando está por expirar)
     String refreshToken(String token);
 }
